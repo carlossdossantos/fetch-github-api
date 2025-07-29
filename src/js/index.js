e342924d03746cb6304b494011582773d6bcd5ba
Carlos Renato <carlos_sdossantos@hotmail.com>
2025-07-29 12:25:54 -0300

    Primeiro commit

---
+++ src/js/index.js
@@ -0,0 +1,57 @@
+import { getUser } from '../services/user.js'
+import { getRepositories } from '../services/repositories.js'
+import { user } from '../objects/user.js'
+import { screen } from '../objects/screen.js'
+
+function validateEmptyInput(userName)
+{
+    if(userName.length === 0)
+    {
+        alert("Preencha o campo com o nome do usuário do GitHub")
+        return true
+    }
+}
+
+document.getElementById("btn-search").addEventListener("click", () => {
+
+    const nameUser = document.getElementById("input-search").value
+    if(validateEmptyInput(nameUser)) return
+    getUserData(nameUser)
+
+})
+
+document.getElementById("input-search").addEventListener("keyup", (e) => {
+
+    const userName = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterKeyPressed = key === 13
+    if (isEnterKeyPressed) {
+        if(validateEmptyInput(userName)) return
+        getUserData(userName)
+    }
+
+})
+
+async function getUserData(userName) {
+
+    const userResponse = await getUser(userName)
+
+    if(userResponse.message === "Not Found")
+    {
+        screen.renderNotFound()
+        return
+    }
+
+    const repositoriesResponse= await getRepositories(userName)
+
+    user.setInfo(userResponse)
+    user.setRepositories(repositoriesResponse)
+
+    //console.log(user)
+
+    screen.renderUser(user)
+}
+
+
+
+
---
+++ src/services/user.js
@@ -0,0 +1,8 @@
+import { baseUrl } from "../js/variable.js"
+
+async function getUser(userName) {
+    const response = await fetch(`${baseUrl}/${userName}`)
+    return await response.json()
+}
+
+export { getUser }
